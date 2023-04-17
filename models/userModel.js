46f4885c32c9180b9f6e0b8b1ea065121f43d9bd
Mostafafathyeid <mostafa180845@feng.bu.edu.eg>
2023-04-17 16:48:20 +0200

    Requests

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,23 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   pic : String,
+ connections: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  sentRequests: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  recievedRequests: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ]
 });
-
 module.exports = mongoose.model("user", userSchema);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
-const { signUp,signIn } = require('../controller/authController')
+const { signUp, signIn, sendRequest, acceptRequest, cancelRequest } = require('../controller/authController')
 router.post('/signup'  ,  signUp)
 router.post('/signin', signIn)
+router.post('/sendrequest', check.isLogin, sendRequest)
+router.post('/acceptrequest', check.isLogin, acceptRequest)
+router.post('/cancelrequest', check.isLogin, cancelRequest)
 module.exports = router;
