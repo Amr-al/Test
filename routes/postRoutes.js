f30d1bfec5aa8997fa07fd783b80ea26771d5b0e
amr mohamed <amrmohamed09@gmail.com>
2023-04-18 16:27:35 +0200

    updates

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { createPost } = require('../controller/postController')
+const { createPost, getPost, addComment } = require('../controller/postController')
 const upload = require('../utilies/multer')
 const check = require('../guard/validation')
 router.post('/create', check.isLogin, upload.Multer.single('post') , createPost)
